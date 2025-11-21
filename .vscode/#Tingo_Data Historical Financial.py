d0d15cead1d4162b4ec0a67a77acc2f60aa1912f
M Group <winstonwilliamsiii@gmail.com>
2025-11-21 17:27:30 -0500

    Refactor Tingo data fetcher by cleaning up commented code and ensuring proper import order

--- .vscode/#Tingo_Data Historical Financial.py
+++ .vscode/#Tingo_Data Historical Financial.py
@@ -1,10 +1,7 @@
 #Tingo_Data Historical Financial Data Fetcher
 
 import pandas_datareader as pdr
-import os
 # Set your Tiingo API key
+import os
 api_key = os.environ.get('TIINGO_API_KEY')
-# Fetch historical data for Apple Inc. (AAPL)
-df = pdr.get_data_tiingo('AAPL', api_key=api_key)
-# Display the last few rows of the data
-print(df.tail())
+# Fetch historical data for Apple Inc.

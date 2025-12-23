e9c2329d992676b9c8b191d8a7704a91be3f6563
M Group <winstonwilliamsiii@gmail.com>
2025-12-23 10:19:58 -0500

    feat: Implement watchlist service with add and get functions

---
+++ appwrite-functions/services_watchlist.py
@@ -0,0 +1,14 @@
+//# services_watchlist
+
+from services.appwrite_client import call_function
+
+def add_to_watchlist(user_id, symbol):
+    return call_function("add_to_watchlist_streamlit", {
+        "user_id": user_id,
+        "symbol": symbol
+    })
+
+def get_watchlist(user_id):
+    return call_function("get_watchlist_streamlit", {
+        "user_id": user_id
+    })

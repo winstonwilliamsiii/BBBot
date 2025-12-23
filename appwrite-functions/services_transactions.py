0b0f2b42228b424e07cbd5055befd83454eff84d
M Group <winstonwilliamsiii@gmail.com>
2025-12-23 10:12:30 -0500

    feat: Add transaction services for creating and retrieving transactions

---
+++ appwrite-functions/services_transactions.py
@@ -0,0 +1,16 @@
+//#services/transactions
+
+from services.appwrite_client import call_function
+
+def create_transaction(user_id, amount, date):
+    return call_function("create_transaction", {
+        "user_id": user_id,
+        "amount": amount,
+        "date": date
+    })
+
+def get_transactions(user_id, limit=100):
+    return call_function("get_transactions_streamlit", {
+        "user_id": user_id,
+        "limit": limit
+    })

0e12d00eb53b2d49a1547c9ca11198a4efa24a43
Winston Williams III <winstonwilliamsiii@users.noreply.github.com>
2026-04-30 12:21:39 -0400

    feat: add IPv4 support for requests in Alpha Vantage API script

--- scripts/test_av_key.py
+++ scripts/test_av_key.py
@@ -1,12 +1,31 @@
-import os, dotenv, requests
+import os, socket, dotenv
+import requests
+from requests.adapters import HTTPAdapter
+from urllib3.util.connection import allowed_gai_family
 
 dotenv.load_dotenv()
 key = os.getenv("ALPHA_VANTAGE_API_KEY", "")
 print("AV key loaded:", (key[:4] + "****") if key else "MISSING")
 
+
+class _IPv4Adapter(HTTPAdapter):
+    def send(self, request, *args, **kwargs):
+        _orig = allowed_gai_family
+        try:
+            import urllib3.util.connection as _c
+            _c.allowed_gai_family = lambda: socket.AF_INET
+            return super().send(request, *args, **kwargs)
+        finally:
+            import urllib3.util.connection as _c
+            _c.allowed_gai_family = _orig
+
+
+session = requests.Session()
+session.mount("https://", _IPv4Adapter())
+
 url = "https://www.alphavantage.co/query"
 params = {"function": "GLOBAL_QUOTE", "symbol": "IYT", "apikey": key}
-r = requests.get(url, params=params, timeout=10)
+r = session.get(url, params=params, timeout=10)
 data = r.json()
 
 if "Global Quote" in data and data["Global Quote"]:
